223a79d2b388c89547c16ef0bf5d3f0f055c1993
Alex <Alex@DESKTOP-50QRJV1>
2024-07-06 07:26:45 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication7/Main.c
@@ -0,0 +1,43 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <windows.h>
+
+// Функція для обчислення факторіалу числа
+long factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    char word[15];
+    printf("Введіть слово: ");
+    scanf("%s", word);
+
+    // Приведення всіх літер до нижнього регістру для правильного обчислення
+    for (int i = 0; i < strlen(word); i++) {
+        word[i] = tolower(word[i]);
+    }
+
+    // Масив для підрахунку кількості кожної букви в слові
+    int x[26] = { 0 };
+    for (int i = 0; i < strlen(word); i++) {
+        x[word[i] - 'a']++;
+    }
+
+    // Обчислення кількості анаграм
+    long total_anagrams = factorial(strlen(word));
+    for (int i = 0; i < 26; i++) {
+        if (x[i] > 1) {
+            total_anagrams /= factorial(x[i]);
+        }
+    }
+
+    printf("Кількість анаграм: %llu\n", total_anagrams);
+    return 0;
+}
